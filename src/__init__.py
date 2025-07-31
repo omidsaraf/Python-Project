e5241489f957dcba503cd56035a13d4281da8bac
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2025-07-31 10:06:37 +0000

    a

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 # src/__init__.py
 
-from .ingest import load_data, validate_data
+from .ingestion import load_data, validate_data
 from .bronze_to_silver import transform_bronze_to_silver
 from .silver_to_gold import transform_silver_to_gold
-from .presentation import generate_visualizations
+from .visualization import generate_visualizations
 
 __all__ = [
     "load_data",
