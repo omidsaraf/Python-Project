35bf4aef3ee9661070e0f3150bc191e6b167ab44
Omid Saraf <omidsarafmanesh.osm@outlook.com>
2025-07-31 10:20:29 +0000

    a

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,15 @@
 # src/__init__.py
 
-from .ingestion import ingest_files, validate_data
+from .ingestion import ingest_files, validate_schema
 from .bronze_to_silver import clean_bronze_to_silver
 from .silver_to_gold import aggregate_and_enrich
 from .visualization import generate_visualizations
 
 __all__ = [
-    "load_data",
-    "validate_data",
-    "transform_bronze_to_silver",
-    "transform_silver_to_gold",
+    "ingest_files",
+    "validate_schema",
+    "clean_bronze_to_silver",
+    "aggregate_and_enrich",
     "generate_visualizations",
 ]
 
